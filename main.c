ac6817a967d8370ad25d23ab1f42c5d8f8964c56
agrmv <gromov-aleksey@outlook.com>
2017-04-14 16:38:35 +0700

    Verification of input data + printf

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
                                                                                  
 int main(int argc, char *argv[])                    
 {         
-
+    if(!argv[1]) {
+        printf("You did not enter anything\n");
+        printf("Expected ./calc \"expression\"\n");
+        exit(-1);
+    }
+    printf("%s\n", argv[1]);
     return 0;
 }
