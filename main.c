90ffb95643290b35e88122b60a5a5f4db2acf632
agrmv <gromov-aleksey@outlook.com>
2017-04-16 23:43:26 +0700

    Added func delete_space

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void deletespace(char *arr);
                                                                                  
 int main(int argc, char *argv[])                    
 {         
@@ -8,6 +10,17 @@ int main(int argc, char *argv[])
         printf("Expected ./calc \"expression\"\n");
         exit(-1);
     }
+    //deletespace(argv[1]);
     printf("%s\n", argv[1]);
     return 0;
 }
+
+/*void deletespace(char *arr)
+{
+    unsigned short i = 0, j = 0;
+    
+    while ((*(arr+i) = *(arr + j++)) != '\0') 
+        if (*(arr+i) != ' ')
+            i++;                   
+    return;
+}*/
