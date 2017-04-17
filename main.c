092c05deeb2f4ce3649635deaf65c4779f430575
agrmv <gromov-aleksey@outlook.com>
2017-04-17 21:30:42 +0700

    Added func plus_minus and number

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-void deletespace(char *arr);
+void deletespace(char *);                    
+float value(char *);                             
+float plusminus(char *, int *);                 
+float number(char *, int *);               
                                                                                  
 int main(int argc, char *argv[])                    
 {         
@@ -10,17 +14,55 @@ int main(int argc, char *argv[])
         printf("Expected ./calc \"expression\"\n");
         exit(-1);
     }
-    //deletespace(argv[1]);
-    printf("%s\n", argv[1]);
+    deletespace(argv[1]);
+    printf("%g\n", value(argv[1]));
     return 0;
 }
 
-/*void deletespace(char *arr)
+void deletespace(char *arr)
 {
-    unsigned short i = 0, j = 0;
+    int i = 0, j = 0;
     
     while ((*(arr+i) = *(arr + j++)) != '\0') 
         if (*(arr+i) != ' ')
             i++;                   
     return;
-}*/
+}
+
+
+float value(char *arr)
+{
+    int index = 0;
+ 
+    return plusminus(arr, &index);
+}
+
+float plusminus(char *arr, int *index)
+{                                 
+    float val = number(arr, index);
+        
+    while (*(arr + *index) == '+' || *(arr + *index) == '-') {                                          
+        switch (*(arr + *index)) {       
+            case '+':
+                ++*index;          
+                val += number(arr, index);
+                break;
+            case '-':
+                ++*index;           
+                val -= number(arr, index);
+                break;                                                    
+        }
+    }
+    return val;
+}
+
+float number(char *arr, int *index)               //обработка строки
+{     
+    float val = 0;  
+                                
+    while (isdigit(*(arr + *index))) {                      //символы в число
+        val = 10 * val + (*(arr + *index) - '0');
+        ++*index;              
+    }
+    return val;
+}    
