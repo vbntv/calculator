dfd40fe207e9f159d3df8e62bd5588f96c8d764c
agrmv <gromov-aleksey@outlook.com>
2017-04-23 13:36:49 +0700

    Many files prog

---
+++ src/calc.h
@@ -0,0 +1,11 @@
+#ifndef CALC_H
+#define CALC_H
+
+void deletespace(char *);                    
+float value(char *);                             
+float plusminus(char *, int *); 
+float muldiv(char *, int *);
+float power(char *, int *);                 
+float number(char *, int *);  
+
+#endif   
