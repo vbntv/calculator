e5fcb940bfd34a35b488a6833e53e6cebc9fe8ff
agrmv <gromov-aleksey@outlook.com>
2017-04-23 15:58:57 +0700

    Some fixed

--- src/calc.h
+++ src/calc.h
@@ -1,13 +1,13 @@
 #ifndef CALC_H
 #define CALC_H
 
-void deletespace(char *);
-void checkbrackets(char *);         
-float value(char *);                             
-float plusminus(char *, int *); 
-float muldiv(char *, int *);
-float power(char *, int *);
-float priority(char *, int *);
-float number(char *, int *);  
+void deletespace(char *);                           //удаление пробелов из строки
+void checkbrackets(char *);                         //определяет правильность введённой скобочной структуры
+float value(char *);                                //получение результата
+float plusminus(char *, unsigned short *);          //сложение "+" вычитание "-"            
+float muldiv(char *, unsigned short *);             //умножение "*" деление "/"
+float power(char *, unsigned short *);              //возведение в степень  "^"
+float priority(char *, unsigned short *);           //приоритет для скобок и проверка на отрицательный префикс
+float number(char *, unsigned short *);             //получение числа 
 
 #endif  
